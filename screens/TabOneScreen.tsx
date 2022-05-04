10e1037d4817166244bd3de08379fbed999726a1
Apoorv Walia <technology@prasukorganics.in>
2022-05-05 00:59:50 +0530

    message screen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,24 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet } from "react-native";
+import  ChatRoomItem  from '../components/ChatRoomItem';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import chatRoomsData from '../assets/dummy-data/ChatRooms';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View style={styles.page}>
+      <FlatList 
+      data={chatRoomsData}
+      renderItem= {({ item }) => <ChatRoomItem chatRoom={item} />  }
+      />
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
+  page: {
+    backgroundColor: 'white',
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
